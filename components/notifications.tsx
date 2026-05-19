d3ad51f1ec879c0762846872b84ebad66f9cf7ec
Saahil Lakhani <saahillakhani@hotmail.co.uk>
2026-05-19 19:11:24 +0100

    Fix push notification type error

--- components/notifications.tsx
+++ components/notifications.tsx
@@ -31,13 +31,14 @@ function isStandalone(): boolean {
   );
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
     .replace(/-/g, "+")
     .replace(/_/g, "/");
   const raw = atob(base64);
-  const arr = new Uint8Array(raw.length);
+  const buffer = new ArrayBuffer(raw.length);
+  const arr = new Uint8Array(buffer);
   for (let i = 0; i < raw.length; i++) arr[i] = raw.charCodeAt(i);
   return arr;
 }
